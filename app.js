d9c5f1d44f66470e305189e126cc68ba573c2b4e
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-03-29 21:01:09 +0200

    Added variables

--- app.js
+++ app.js
@@ -4,3 +4,25 @@
 fs = require('fs');
 readline = require('readline');
 
+// 
+// Variables
+// 
+var chat;
+var removes = [
+    "Nachrichten und Anrufe sind Ende-zu-Ende-verschlüsselt.",
+    "Tippe, um mehr zu erfahren.",
+    "Audio weggelassen",
+    "Video weggelassen",
+    "Verpasster Gruppen-",
+    "Verpasster Videoanruf",
+    "Verpasster Sprachanruf",
+    "Du hast diese Nachricht gelöscht.",
+    "Du hast diesen Anruf gelöscht.",
+    "Bild weggelassen",
+    "Sticker weggelassen",
+    "Diese Nachricht wurde gelöscht.",
+    "Dokument weggelassen",
+    "GIF weggelassen"
+];
+var skip_line = false;
+
